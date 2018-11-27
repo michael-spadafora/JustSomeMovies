3ee5609761f0f89cb4d8c657d16c5fe0bb4e9b19
Michael Spadafora <michael.spadafora@stonybrook.edu>
2018-11-26 23:40:19 -0500

    planning movies and actors pages

---
+++ pages/actors.js
@@ -0,0 +1,4 @@
+//what we want here:
+//select * from actors
+//sort by name, age, salary etc dropdown menu
+//
---
+++ pages/movies.js
@@ -0,0 +1,4 @@
+//what we want here:
+//select * from actors
+//sort by name, rating, genre etc dropdown menu
+//browsing: only show movies with stars from x to y, only show movies of genre z
