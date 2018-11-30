f149113361174ca354ea300a6609e74e402f356b
Jackie Quach <jackiequach14@gmail.com>
2018-11-30 02:39:06 -0500

    Edit persons to actors

--- server.js
+++ server.js
@@ -7,16 +7,17 @@ const cors = require('cors');
 const app = express();
 
 var con = mysql.createConnection({
-host: "localhost",
-user: "root",
-password: "root",
-database: "sbudb"
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "sbudb"
 });
 
 con.connect(err => {
     if(err) throw err;
 });
 
+app.use(cors()); // Will be used for access control in the future
 
 app.get('/movies', (req, res) => {
     // Takes SQL query and creates a GET method route to send data to /movies path
@@ -33,13 +34,13 @@ app.get('/movies', (req, res) => {
     });
 });
 
-app.get('/persons', (req, res) => {
-    con.query('SELECT * FROM person', (error,results) => {
+app.get('/actors', (req, res) => {
+    con.query('SELECT * FROM person p INNER JOIN actors a ON p.p_id = a.actor_id', (error,results) => {
         if(error) 
             return res.send(error);
         else {
             return res.json({
-                persons: results
+                actors: results
             })
         }
     });
@@ -62,16 +63,30 @@ app.get('/a/:id',function(req,res) {
         if(err)
             return res.send(err);
         else {
-            return res.json({
-                actor: results
+            con.query('SELECT m.movie_id AS movie_id, m.title AS title, m.poster_url AS poster_url, ai.actor_role AS actor_role FROM movies m INNER JOIN acts_in ai ON m.movie_id = ai.movie_id INNER JOIN person p ON p.p_id = ai.actor_id WHERE p_id = ?;', req.params.id, function(err,results2){
+                if(err)
+                    return res.send(err);
+                else { 
+                    return res.json({
+                        actor: results, movies: results2
+                    });
+                }
             });
         }
     });
 });
 
-app.get('*',(req,res)=> {
-    return handle(req,res);
-})
+app.get('/m/:id',function(req,res) {
+    con.query('SELECT * FROM movies WHERE movie_id = ?', req.params.id, function(err,results){
+        if(err)
+            return res.send(err);
+        else {
+            return res.json({
+                movie: results
+            });
+        }
+    });
+});
 
 app.listen(4000, () => {
     // Waits for a request from client 
