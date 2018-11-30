212b86d771c14355bb87c9cf7ffaac05df982a54
Jackie Quach <jackiequach14@gmail.com>
2018-11-29 22:40:16 -0500

    Add all actors

--- pages/actors.js
+++ pages/actors.js
@@ -2,3 +2,27 @@
 //select * from actors
 //sort by name, age, salary etc dropdown menu
 //
+
+import Link from 'next/link'
+import HeaderLayout from '../components/HeaderLayout'
+import SortByMovie from '../components/SortByMovie'
+import Person from '../components/Person'
+
+const actors = (props) => (
+    <div>
+        <HeaderLayout head/>
+        <div style={{display:"inline-block",fontSize:"23pt",fontWeight:"bold",paddingLeft:"10px"}}>Actors</div><div style={{float:"right"}}><SortByMovie /></div>
+        <Person />
+        <link href="https://fonts.googleapis.com/css?family=Nunito:900:300" rel="stylesheet"/>
+        <style jsx>{`
+            body{
+                background-color: #f2f2f2;
+            }
+            div {
+                font-family:Nunito; 
+            }
+        `}</style>
+    </div>
+  )
+  
+  export default actors
