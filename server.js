1369442fa431f7c03a2de60e208fee3fa1c1b708
Jackie Quach <jackiequach14@gmail.com>
2018-12-02 22:33:19 -0500

    Add queries for movie info

--- server.js
+++ server.js
@@ -95,11 +95,11 @@ app.get('/sliderImages', (req,res) => {
 });
 
 app.get('/a/:id',function(req,res) {
-    con.query('SELECT * FROM person WHERE p_id = ?', req.params.id, function(err,results){
+    con.query('SELECT *, DATE_FORMAT(dob,"%M %d, %Y") AS dob FROM person WHERE p_id = ?', req.params.id, function(err,results){
         if(err)
             return res.send(err);
         else {
-            con.query('SELECT m.movie_id AS movie_id, m.title AS title, m.poster_url AS poster_url, ai.actor_role AS actor_role FROM movies m INNER JOIN acts_in ai ON m.movie_id = ai.movie_id INNER JOIN person p ON p.p_id = ai.actor_id WHERE p_id = ?;', req.params.id, function(err,results2){
+            con.query('SELECT m.movie_id, m.title, m.poster_url, ai.actor_role FROM movies m INNER JOIN acts_in ai ON m.movie_id = ai.movie_id INNER JOIN person p ON p.p_id = ai.actor_id WHERE p_id = ?;', req.params.id, function(err,results2){
                 if(err)
                     return res.send(err);
                 else { 
@@ -113,12 +113,48 @@ app.get('/a/:id',function(req,res) {
 });
 
 app.get('/m/:id',function(req,res) {
-    con.query('SELECT * FROM movies WHERE movie_id = ?', req.params.id, function(err,results){
+    con.query('SELECT *, DATE_FORMAT(release_date,"%M %d, %Y") AS release_date FROM movies WHERE movie_id = ?', req.params.id, function(err,results){
         if(err)
             return res.send(err);
         else {
-            return res.json({
-                movie: results
+            con.query('SELECT p.p_id ,p.p_name,p.img_url, ai.actor_role FROM movies m INNER JOIN acts_in ai ON m.movie_id = ai.movie_id INNER JOIN person p ON p.p_id = ai.actor_id WHERE m.movie_id = ?;', req.params.id, function(err,results2){
+                if(err)
+                    return res.send(err);
+                else {
+                    con.query('SELECT p.p_id, p.p_name, p.img_url FROM movies m INNER JOIN directs d ON m.movie_id = d.movie_id INNER JOIN person p ON p.p_id = d.director_id WHERE m.movie_id = ?;', req.params.id, function(err,results3){
+                        if(err)
+                            return res.send(err);
+                        else {
+                            con.query('SELECT p.p_id, p.p_name, p.img_url FROM movies m INNER JOIN produces pr ON m.movie_id = pr.movie_id INNER JOIN person p ON p.p_id = pr.producer_id WHERE m.movie_id = ?;', req.params.id, function(err,results4){
+                                if(err)
+                                    return res.send(err);
+                                else {
+                                    con.query('SELECT p.p_id, p.p_name, p.img_url, GROUP_CONCAT(wo.member_role SEPARATOR ", ") AS member_role FROM movies m INNER JOIN works_on wo ON m.movie_id = wo.movie_id INNER JOIN person p ON p.p_id = wo.production_team_member_id WHERE m.movie_id = ? GROUP BY p.p_id;', req.params.id, function(err,results5){
+                                        if(err)
+                                            return res.send(err);
+                                        else { 
+                                            con.query('SELECT d.dist_id, d.dist_name, d.location FROM movies m INNER JOIN distributions di ON di.movie_id = m.movie_id INNER JOIN distributors d ON d.dist_id = di.dist_id WHERE m.movie_id = ?;', req.params.id, function(err,results6){
+                                                if(err)
+                                                    return res.send(err);
+                                                else { 
+                                                    con.query('SELECT GROUP_CONCAT(g.genre SEPARATOR ", ") AS genres FROM movies m INNER JOIN movie_genres mg ON m.movie_id=mg.movie_id INNER JOIN genres g ON g.genre_id=mg.genre_id WHERE m.movie_id = 1 GROUP BY m.movie_id;', req.params.id, function(err,results7){
+                                                        if(err)
+                                                            return res.send(err);
+                                                        else { 
+                                                            return res.json({
+                                                                movie: results, actors: results2, directors: results3, producers: results4, crew: results5, distributors: results6, genres: results7
+                                                            });
+                                                        }
+                                                    });
+                                                }
+                                            });
+                                        }
+                                    });
+                                }
+                            });
+                        }
+                    });
+                }
             });
         }
     });
