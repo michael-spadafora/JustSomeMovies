bbaf11253aad0730a1ab5f97a477fd0cc4d4a87c
Michael Spadafora <michael.spadafora@stonybrook.edu>
2018-12-05 22:49:32 -0500

    fixed sort by rating

--- pages/index.js
+++ pages/index.js
@@ -11,17 +11,27 @@ const Index = (props) => (
         <link href="https://fonts.googleapis.com/css?family=Nunito:900:300" rel="stylesheet"/>
       </Head>
     	<HeaderLayout head/>
-      <Slider top = {80} left = {20} height = {500} width = {780} /> 
+      <div className = 'centeredContainer'>
+        <div className = 'sliderDiv'>
+          <Slider top = {80} left = {250} height = {500} width = {780} /> 
+        </div>
+      </div>
       {/* //values are all adjustable */}
       {/* we need to replace these with other images for better sizing */}
 
-      <Slider top = {80} left = {940} height = {600} width =  {400}/>
+      {/* <Slider top = {80} left = {940} height = {600} width =  {400}/> */}
 
       {/* we need to replace these with other images for better sizing */}
       {/* //values are all adjustable */}
       <style jsx global> {`
         body {
          background-color: #f2f2f2;
+        }
+        .sliderDiv {
+          margin: 0 auto
+        }
+        .centeredContainer {
+          text-align: center;
         }
       `}</style>
     </div>
